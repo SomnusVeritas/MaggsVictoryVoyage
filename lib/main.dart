175ee991bb7fa46019e0922d6a2ba74af1c1264d
SomnusVeritas <sites@skup.in>
2023-10-20 23:22:21 +0200

    added provider to main

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:maggs_victory_voyage/services/feed_provider.dart';
 import 'package:provider/provider.dart';
 import 'package:maggs_victory_voyage/services/db_helper.dart';
 import 'pages/splash_page.dart';
+import 'services/profiles_provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,6 +13,9 @@ void main() async {
       providers: [
         ChangeNotifierProvider(
           create: (_) => Feed(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => Profiles(),
         )
       ],
       child: const Application(),
